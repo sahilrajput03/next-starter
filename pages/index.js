c5c30cc52d827e409c683d155cbb11079d26c643
Sahil Rajput <sahilrajput03@gmail.com>
2021-03-22 17:10:41 +0530

    clean code. ~ sahil

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,47 @@
-import styles from "../styles/Home.module.css";
+import {useEffect, useState} from 'react'
+import styles from '../styles/Home.module.css'
+import useSWR from 'swr'
 
-export default function Home() {
-  return <div className={styles.container}>Hello, from nextjs!</div>;
+let {log} = console
+const fetcher = (...args) => fetch(...args).then((res) => res.json())
+
+export default function HomePage() {
+  return (
+    <>
+      <UsingFetch />
+      <br />
+      <UsingSwr />
+    </>
+  )
+}
+
+function UsingSwr() {
+  const {data, error} = useSWR('/api/hello', fetcher)
+  log(data)
+
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+  return <div>Hello {data.name}!(from swr)</div>
+}
+
+function UsingFetch() {
+  const [state, setState] = useState()
+  useEffect(() => {
+    const fun = async () => {
+      const json = await (await fetch('/api/hello')).json()
+      // await new Promise((resolve) => setTimeout(resolve, 2000))
+      setState(json)
+    }
+    fun()
+  }, [])
+
+  console.log()
+
+  if (!state) return 'Loading...'
+
+  return (
+    <div className={styles.container}>
+      Hello, {state.name} (from builit fetch library in nextjs.)
+    </div>
+  )
 }
