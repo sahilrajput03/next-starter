3b4bcc58bd1e3f726d790d44e99693e3b51a47a5
Sahil Rajput <sahilrajput03@gmail.com>
2021-04-17 14:32:14 +0530

    Add usage importing css files using nextjs config and installing dependency - @zeit/next-css. ~ sahil

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import "../styles/globals.css";
+// Global imports are the coolest thing as it mimics the behaviour of old-school, and i love old school.
+// Also, you can add multiple global sheets, yikes!
+function MyApp({Component, pageProps}) {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
